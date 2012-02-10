4117093bff5a72cfa88681c4dcda27ec2441942c
Nathanael Anderson <nathanael.anderson@kellpro.com>
2012-02-10 11:01:10 -0600

    Update for TextArea

--- src/uki-view/view/nativeControl.js
+++ src/uki-view/view/nativeControl.js
@@ -30,7 +30,7 @@ var NativeControl = view.newClass('NativeControl', Base, Focusable, {
 
 });
 fun.delegateProp(NativeControl.prototype,
-    ['name', 'checked', 'disabled', 'value', 'type'], '_input');
+    ['name', 'checked', 'disabled', 'value', 'type', 'accessKey'], '_input');
 
 
 
@@ -96,12 +96,14 @@ var Text = view.newClass('nativeControl.Text', NativeControl, {
 
   placeholder: fun.newProp('placeholder', function(v) {
     this._placeholder = v;
-    if (this._input.placeholder !== undefined) {
+  /*  if (this._input.placeholder !== undefined) {
+      this._input.alt = v;
+      this._input.title = v;
       this._input.placeholder = v;
-    } else {
+    } else { */
       this._initPlaceholder();
       this._placeholderDom.innerHTML = dom.escapeHTML(v);
-    }
+   // }
   }),
 
   _layout: function() {
@@ -123,7 +125,7 @@ var Text = view.newClass('nativeControl.Text', NativeControl, {
     evt.on(this._placeholderDom, 'click', fun.bindOnce(function() {
       this.focus();
     }, this));
-    this.on('focus blur change keyup', this._updatePlaceholderVis);
+    //this.on('focus blur change keyup', this._updatePlaceholderVis);
     if (this._input.offsetHeight) {
       this._updatePlaceholderHeight();
     }
@@ -145,47 +147,131 @@ var Text = view.newClass('nativeControl.Text', NativeControl, {
         targetStyle[name] = sourceStyle[name];
       }
     });
-    targetStyle.lineHeight = this._input.offsetHeight +
-        (parseInt(sourceStyle.marginTop, 10) || 0)*2 + 'px';
+    targetStyle.lineHeight = (this._input.offsetHeight + (parseInt(sourceStyle.marginTop, 10) || 0)*2) + 'px';
     targetStyle.marginLeft = (parseInt(sourceStyle.marginLeft, 10) || 0) +
         (parseInt(sourceStyle.borderLeftWidth, 10) || 0) + 'px';
-    textProto._updatePlaceholderHeight = fun.FS;
+    targetStyle.width = (parseInt(sourceStyle.width)-3) + 'px';
+    targetStyle.textAlign = "right";
+
+   // textProto._updatePlaceholderHeight = fun.FS;
   }
 });
 
 
 /**
-* Text input
-* build({ view: 'nativeControl.TextArea', value: 'John Smith', rows: 6 })
-*/
+ * Text input
+ * build({ view: 'nativeControl.TextArea', value: 'John Smith', placeholder: 'Name?', rows: 7 })
+ */
+
 var TextArea = view.newClass('nativeControl.TextArea', NativeControl, {
 
+  _createDom: function(initArgs) {
+    this._input = dom.createElement('textarea',
+        { className: 'uki-nc-textarea__input', type: 'text' });
+    this._dom = dom.createElement(initArgs.tagName || 'span',
+        { className: 'uki-nc-textarea' });
+    this.dom().appendChild(this._input);
+  },
+
+  rows: fun.newProp('rows', function(v) {
+    this._input.rows = v;
+  }),
+
+  cols: fun.newProp('cols', function(v) {
+    this._input.style.width = "auto";
+    this._dom.style.width = "auto";
+    this._input.cols = v;
+  }),
+
+  width: fun.newProp('width', function(v) {
+     this._input.style.width = v;
+     this._dom.style.width = v;
+  }),
+
+  height: fun.newProp('height', function(v) {
+        this._input.style.height = v;
+        this._dom.style.height = v;
+  }),
+
+  placeholder: fun.newProp('placeholder', function(v) {
+    this._placeholder = v;
+  /*  if (this._input.placeholder !== undefined) {
+      this._input.alt = v;
+      this._input.title = v;
+      this._input.placeholder = v;
+    } else { */
+      this._initPlaceholder();
+      this._placeholderDom.innerHTML = dom.escapeHTML(v);
+    //}
+  }),
+
+  _layout: function() {
+    this._updatePlaceholderHeight();
+    // manual resize box-sizing: border-box for ie 7
+    if (ieResize) {
+      this._input.style.width = this.dom().offsetWidth - 6;
+    }
+  },
+
+  _initPlaceholder: function() {
+    if (this._initedPlaceholder) return;
+
+    this._initedPlaceholder = true;
+    this.addClass('uki-nc-textarea_with-placeholder');
+    this._placeholderDom = dom.createElement('span',
+        { className: 'uki-nc-textarea__placholder' });
+    this.dom().insertBefore(this._placeholderDom, this.dom().firstChild);
+    evt.on(this._placeholderDom, 'click', fun.bindOnce(function() {
+      this.focus();
+    }, this));
+   // this.on('focus blur change keyup', this._updatePlaceholderVis);
+    /*if (this._input.offsetHeight) {
+      this._updatePlaceholderHeight();
+    }*/
+  },
+
+  _updatePlaceholderVis: function() {
+    this._placeholderDom.style.display =
+        (this.hasFocus() || this.value()) ? 'none' : '';
+  },
+
+  _updatePlaceholderHeight: function() {
+    if (!this._placeholderDom) return;
+    var targetStyle = this._placeholderDom.style,
+        sourceStyle = dom.computedStyle(this._input);
+
+    utils.forEach(['font', 'fontFamily', 'fontSize',
+      'paddingLeft', 'paddingTop', 'padding'], function(name) {
+      if (sourceStyle[name] !== undefined) {
+        targetStyle[name] = sourceStyle[name];
+      }
+    });
+    //targetStyle.lineHeight =
+    targetStyle.marginTop = ((this._input.offsetHeight + (parseInt(sourceStyle.marginTop, 10) || 0)*2) - 16)
+        + 'px'; //targetStyle.lineHeight;
+    targetStyle.width = (parseInt(sourceStyle.width)-3) + "px";
+    targetStyle.marginLeft = (parseInt(sourceStyle.marginLeft, 10) || 0) + (parseInt(sourceStyle.borderLeftWidth, 10) || 0) + 'px';
+    targetStyle.textAlign = "right";
+    //textProto._updatePlaceholderHeight = fun.FS;
+  }
+});
+
+
+/**
+ * Image Class
+ */
+var Image = view.newClass('nativeControl.Image', NativeControl, {
     _createDom: function(initArgs) {
-        this._input = dom.createElement('textarea',
-            { className: 'uki-nc-textarea__input', type: 'text' });
-        this._dom = dom.createElement(initArgs.tagName || 'span',
-            { className: 'uki-nc-textarea'});
-        this.dom().appendChild(this._input);
+        this._dom = this._input = dom.createElement('img',
+            { className: 'uki-nc-image' });
     },
 
-    rows: fun.newProp('rows', function(v) {
-      this._input.rows = v;
-    }),
-
-    cols: fun.newProp('cols', function(v) {
-      this._input.style.width = "auto";
-      this._dom.style.width = "auto";
-      this._input.cols = v;
-    }),
-
-
-    _layout: function() {
-        if (ieResize) {
-            this._input.style.width = this.dom().offsetWidth - 6;
-        }
-    }
-
+    height: fun.newProp('height', function (val) {
+        if (arguments.length) this._input.style.height = val;
+        return this._input.style.height;
+    })
 });
+fun.delegateProp(NativeControl.prototype,  ['src'], '_input');
 
 
 /**
@@ -197,7 +283,11 @@ var Button = view.newClass('nativeControl.Button', NativeControl, {
     _createDom: function(initArgs) {
         this._dom = this._input = dom.createElement('input',
             { className: 'uki-nc-button', type: 'button' });
-    }
+    },
+    width: fun.newProp('width', function(v) {
+        this._input.style.width = v;
+    })
+
 });
 
 
@@ -258,7 +348,7 @@ function appendOptions (root, options) {
 
 require('../../uki-core/collection').Collection.addProps([
     'name', 'checked', 'disabled', 'value', 'type', 'placeholder',
-    'disabled', 'options', 'selectedIndex'
+    'disabled', 'options', 'selectedIndex', 'src',
 ]);
 exports.nativeControl = {
     NativeControl: NativeControl,
@@ -267,5 +357,6 @@ exports.nativeControl = {
     Text:          Text,
     Button:        Button,
     Select:        Select,
-    TextArea:      TextArea
+    TextArea:      TextArea,
+    Image:         Image
 };
